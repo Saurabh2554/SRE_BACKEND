adefdcc44639575180056f192de78db22cf4ff13
sanket jojan <mailtosanketjojan@gmail.com>
2024-09-15 21:58:31 +0530

    Schdeulaar tasks

--- ApiMonitoring/Model/ApiMonitoringModel/graphQl/assertion_and_limit_helpers.py
+++ ApiMonitoring/Model/ApiMonitoringModel/graphQl/assertion_and_limit_helpers.py
@@ -55,101 +55,114 @@ def checkAssertion(serviceId, metriceId, response):
     # service = get_service(serviceId=serviceId)
     metrices = get_metrices(metriceId=metriceId)
     assertions = get_assertions_for_service(serviceId=serviceId)
+    try:
 
-    for assertion in assertions:
-        actual_value = None
-        status = False  # Default status
 
-        # Status Code Assertions
-        if assertion.source == "status_code":
-            actual_value = metrices.statusCode
-            expected = int(assertion.expectedValue) if assertion.expectedValue is not None else None
+        for assertion in assertions:
+            actual_value = None
+            status = False  # Default status
 
-            if expected is not None:
-                comparison_operators = {
-                    "equals": actual_value == expected,
-                    "not_equals": actual_value != expected,
-                    "greater_than": actual_value > expected,
-                    "less_than": actual_value < expected,
-                }
-                status = comparison_operators.get(assertion.operator, False)
+            # Status Code Assertions
+            if assertion.source == "status_code":
+                actual_value = metrices.statusCode
+                expected = int(assertion.expectedValue) if assertion.expectedValue is not None else None
 
-        # JSON Body Assertions
-        elif assertion.source == "json_body":
-            content_type = response.headers.get("Content-Type", "").lower()
-            if "json" in content_type:
-                try:
-                    parsed_response = response.json()
-                    extracted_values = getValueFromJSON(parsed_response, assertion.property)
-                except Exception as e:
-                    raise GraphQLError(f"Failed to parse JSON response: {e}")
+                if expected is not None:
+                    comparison_operators = {
+                        "equals": actual_value == expected,
+                        "not_equals": actual_value != expected,
+                        "greater_than": actual_value > expected,
+                        "less_than": actual_value < expected,
+                    }
+                    status = comparison_operators.get(assertion.operator, False)
 
-                expected_value = assertion.expectedValue
+            # JSON Body Assertions
+            elif assertion.source == "json_body":
+                content_type = response.headers.get("Content-Type", "").lower()
+                if "json" in content_type:
+                    try:
+                        parsed_response = response.json()
+                        extracted_values = getValueFromJSON(parsed_response, assertion.property)
+                    except Exception as e:
+                        raise GraphQLError(f"Failed to parse JSON response: {e}")
+
+                    expected_value = assertion.expectedValue
+                    operator = assertion.operator
+
+                    if extracted_values:
+                        for extracted_value in extracted_values:
+                            converted_expected = try_convert(expected_value, type(extracted_value))  # Fixed here
+
+                            # Boolean condition map
+                            condition_map = {
+                                "equals": extracted_value == converted_expected,
+                                "not_equals": extracted_value != converted_expected,
+                                "greater_than": converted_expected is not None and extracted_value > converted_expected,
+                                "less_than": converted_expected is not None and extracted_value < converted_expected,
+                                "contains": str(expected_value) in str(extracted_value),
+                                "not_contains": str(expected_value) not in str(extracted_value),
+                                "is_empty": extracted_value in ["", None],
+                                "is_not_empty": extracted_value not in ["", None],
+                                "is_null": extracted_value is None,
+                                "is_not_null": extracted_value is not None,
+                            }
+
+                            if condition_map.get(operator, False):
+                                status = True
+                                actual_value = extracted_value
+                                break 
+                            else:
+                                actual_value = extracted_value
+
+            elif assertion.source == 'headers':
+                print("Inside Headers")
                 operator = assertion.operator
+                actual_value = response.headers.get(assertion.property,None)
+                print(f'Actula value of headers  : {actual_value}')
 
-                if extracted_values:
-                    for extracted_value in extracted_values:
-                        converted_expected = try_convert(expected_value, type(extracted_value))  # Fixed here
+                if actual_value is not None and assertion.regex:
+                    match = re.search(assertion.regex, actual_value)
+                    print(f"match groups :,{match}")
+                    
+                    
+                    if match:
+                        print(f"match groups :,{match.groups()} : {match.group(0)}")
+                        
+                        actual_value = match.group(0)
+                    else:
+                        actual_value = None
+                
+                if actual_value is not None:
+                    print(f"assertion Expected Type : {assertion.expectedValue}, {actual_value}")
+                    
+                    converted_expected = try_convert(assertion.expectedValue, type(actual_value))
 
-                        # Boolean condition map
-                        condition_map = {
-                            "equals": extracted_value == converted_expected,
-                            "not_equals": extracted_value != converted_expected,
-                            "greater_than": converted_expected is not None and extracted_value > converted_expected,
-                            "less_than": converted_expected is not None and extracted_value < converted_expected,
-                            "contains": str(expected_value) in str(extracted_value),
-                            "not_contains": str(expected_value) not in str(extracted_value),
-                            "is_empty": extracted_value in ["", None],
-                            "is_not_empty": extracted_value not in ["", None],
-                            "is_null": extracted_value is None,
-                            "is_not_null": extracted_value is not None,
-                        }
+                    condition_map = {
+                        "equals": actual_value == converted_expected,
+                        "not_equals": actual_value != converted_expected,
+                        "greater_than": converted_expected is not None and actual_value > converted_expected,
+                        "less_than" : converted_expected is not None and actual_value < converted_expected,
+                        "contains" : assertion.expectedValue in str(actual_value),
+                        "not_contains" : assertion.expectedValue not in str(actual_value),
+                        "is_empty" : actual_value in ["",None],
+                        "is_not_empty" : actual_value not in ["", None],
+                        "is_null" : actual_value is None,
+                        "is_not_null" : actual_value is not None,
+                    }
+                    if condition_map.get(operator,False):
+                        status = True
+                                
 
-                        if condition_map.get(operator, False):
-                            status = True
-                            actual_value = extracted_value
-                            break 
-                        else:
-                            actual_value = extracted_value
+            # Store assertion result
+            assertion_result = AssertionAndLimitResult.objects.create(
+                assertion_and_limit=assertion,
+                apimetrics=metrices,
+                actual_value=actual_value,
+                status=status
+            )
+    
 
-        elif assertion.source == 'headers':
-            
-            operator = assertion.operator
-            actual_value = response.headers.get(assertion.property,None)
-
-            if actual_value is not None and assertion.regex:
-                match = re.search(assertion.regex, actual_value)
-                if match:
-                    actual_value = match.group(1)
-                else:
-                    actual_value = None
-            
-            if actual_value is not None:
-                converted_expected = try_convert(assertion.expectedValue, type(actual_value))
-
-                condition_map = {
-                    "equals": actual_value == converted_expected,
-                    "not_equals": actual_value != converted_expected,
-                    "greater_than": converted_expected is not None and actual_value > converted_expected,
-                    "less_than" : converted_expected is not None and actual_value < converted_expected,
-                    "contains" : assertion.expectedValue in str(actual_value),
-                    "not_contains" : assertion.expectedValue not in str(actual_value),
-                    "is_empty" : actual_value in ["",None],
-                    "is_not_empty" : actual_value not in ["", None],
-                    "is_null" : actual_value is None,
-                    "is_not_null" : actual_value is not None,
-                }
-            if condition_map.get(operator,False):
-                status = True
-                            
-
-        # Store assertion result
-        assertion_result = AssertionAndLimitResult.objects.create(
-            assertion_and_limit=assertion,
-            apimetrics=metrices,
-            actual_value=actual_value,
-            status=status
-        )
-
-        print(f"Assertion Result: {assertion_result}")
+            print(f"Test this Assertion Result: {assertion_result}")
+    except Exception as e :
+        print(f"Texas Image Error Regex: {e}")
 
