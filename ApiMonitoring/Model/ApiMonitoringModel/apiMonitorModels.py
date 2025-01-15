e5b7a2ae18bc120c5072fbc1f6435bb6f95bde8f
Saurabh2554 <145126579+Saurabh2554@users.noreply.github.com>
2025-01-15 18:57:05 +0530

    Add SchedulingAndAlerting model, AssertionAndLimit model

--- ApiMonitoring/Model/ApiMonitoringModel/apiMonitorModels.py
+++ ApiMonitoring/Model/ApiMonitoringModel/apiMonitorModels.py
@@ -3,7 +3,7 @@ from django.db import models
 # from ApiMonitoring.Model.AuthTypeModel.authConfigModels import Authentication
 from Business.models import BusinessUnit , SubBusinessUnit
 from django.utils import timezone
-from django_celery_beat.models import PeriodicTask, CrontabSchedule
+from django_celery_beat.models import PeriodicTask
 import os
 from dotenv import load_dotenv
 
@@ -18,18 +18,10 @@ class MonitoredAPI(models.Model):
     id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     businessUnit = models.ForeignKey(BusinessUnit ,on_delete = models.CASCADE)
     subBusinessUnit = models.ForeignKey(SubBusinessUnit ,on_delete = models.CASCADE)
+
     apiName = models.CharField(max_length=255, null=True, blank=True)  # API name
     apiUrl = models.URLField()  # Common URL field for both REST and GraphQL
-    
-    
-    # Monitoring settings
-    apiCallInterval = models.IntegerField(default=5, null=True, blank=True)  # Monitoring interval in min
-    expectedResponseTime = models.IntegerField(default=10, null=True, blank=True)  # Request timeout in ms
     headers = models.JSONField(null=True, blank=True)  # Optional custom headers
-
-    # Link to authentication system
-    # authentication = models.ForeignKey(Authentication, on_delete=models.SET_NULL, null=True, blank=True)  # Foreign key to Authentication
-    
     methodType = models.CharField(max_length=10, choices=METHOD_TYPE_CHOICES)
     requestBody = models.TextField(null = True, blank = True) #
 
@@ -39,13 +31,7 @@ class MonitoredAPI(models.Model):
     # Timestamps
     createdAt = models.DateTimeField(default=timezone.now)
     
-    # owner
-    maxRetries = models.IntegerField(default = 3, null=True, blank=True)
-    retryAfter = models.IntegerField(default = 60, null = True, blank = True)
-    recipientDl = models.JSONField(null = False , blank = False)
-    createdBy = models.EmailField(null = True , blank = True)
-    teamsChannelWebhookURL = models.TextField(default = os.getenv('TEAMS_CHANNEL_WEBHOOK_URL'))
-
+    # task_related
     taskId = models.ForeignKey(PeriodicTask, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
--- ApiMonitoring/Model/ApiMonitoringModel/assertionAndLimitModels.py
+++ ApiMonitoring/Model/ApiMonitoringModel/assertionAndLimitModels.py
@@ -1,9 +1,12 @@
+import uuid
 from django.db import models
+from ApiMonitoring.Model.ApiMonitoringModel.apiMonitorModels import MonitoredAPI
 
 class AssertionAndLimit(models.Model):
-    expectedResponseTime = models.IntegerField(default=10, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    api = models.ForeignKey(MonitoredAPI, on_delete=models.CASCADE, related_name='assertionAndLimit')
     degradedResponseTime = models.IntegerField(default=10, null=True, blank=True)
     failedResponseTime = models.IntegerField(default=10, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.expectedResponseTime}"
+        return f"{self.degradedResponseTime}"
